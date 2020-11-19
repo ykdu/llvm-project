e407b2c2de9c2c519439b438b3e5699fb2730ab8
duyunkai <jordon0811@163.com>
2020-11-19 15:38:09 +0800

    format warning

--- clang/lib/StaticAnalyzer/Checkers/RPCChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/RPCChecker.cpp
@@ -288,13 +288,11 @@ void CrossCompilationUnit::crossUnitCheck(std::map<const Expr*, bool> visitedRPC
         }
         auto serverSet = iter->second;
 
-        std::set<std::string> clientOnly, serverOnly, csCommon;
+        std::set<std::string> clientOnly, serverOnly;
         std::set_difference(clientSet.begin(), clientSet.end(), serverSet.begin(), serverSet.end(),
                     std::inserter(clientOnly, clientOnly.end()));
         std::set_difference(serverSet.begin(), serverSet.end(), clientSet.begin(), clientSet.end(),
                     std::inserter(serverOnly, serverOnly.begin()));
-        std::set_intersection(clientSet.begin(), clientSet.end(), serverSet.begin(), serverSet.end(),
-                    std::inserter(csCommon, csCommon.end()));
         if(!clientOnly.empty() || !serverOnly.empty()) {
             llvm::errs() << "warning: Unique path [core.RPC]\n";
             for(const auto &path: clientOnly) {
@@ -303,9 +301,15 @@ void CrossCompilationUnit::crossUnitCheck(std::map<const Expr*, bool> visitedRPC
             for(const auto &path: serverOnly) {
                 llvm::errs() << "    server:"   << name << "'s path ---> " << path << "\n";
             }
-            llvm::errs() << "    where common pathes are:\n";
-            for(const auto &path: csCommon) {
-                llvm::errs() << "        " << path << "\n";
+
+            std::set<std::string> csCommon;
+            std::set_intersection(clientSet.begin(), clientSet.end(), serverSet.begin(), serverSet.end(),
+                        std::inserter(csCommon, csCommon.end()));
+            if(!csCommon.empty()) {
+                llvm::errs() << "    where common pathes are:\n";
+                for(const auto &path: csCommon) {
+                    llvm::errs() << "        " << path << "\n";
+                }
             }
         }
 
